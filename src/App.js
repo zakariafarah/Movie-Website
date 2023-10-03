d2c4d4e24f9d11c23e20ecbe179cad65dfb4356c
Zakaria Farah <zakaria.farah99@gmail.com>
2023-10-02 23:01:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
+//URL
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
